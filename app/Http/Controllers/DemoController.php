29084349ca1c3728d66b0f3c76f4b80e8f51b23e
Shourav Saha <shourav939@gmail.com>
2024-01-08 23:01:12 +0600

    just practice laravel query builder methods

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -2,19 +2,31 @@
 
 namespace App\Http\Controllers;
 // use illuminate\Support\Collection;
+
+use Illuminate\Database\Query\JoinClause;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class DemoController extends Controller
 {
     //
-    public function DemoAction(){
-        $result = DB::table('product_sliders')
-        ->join('products', 'product_sliders.product_id', '=', 'product_sliders.id')
-        ->get();
+    // public function DemoAction(){
+    //     $advancedJoin = DB::table('product_carts')
+    //     ->join('products', function(JoinClause $join){
+    //     $join->on('product_carts.product_id', '=', 'products.id')
+    //     ->where('product_carts.color', '=', 'red');
+    //     })->get();
+    //     return $advancedJoin;
+    // }
+
+    // Basic where cluases
+    // = (equal to)
+    // != (not equal)
+
+    public function unionFunction(){
+        $query1 = DB::table('product_carts')->where('size', '=', 'x');
+        $query2 = DB::table('product_carts')->where('product_id', '>', '4');
+        $result = $query2->union($query1)->get();
         return $result;
-        }
-
-
     }
-
+}
