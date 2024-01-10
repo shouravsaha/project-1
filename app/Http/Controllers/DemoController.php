c7f721ffaa686e29767445379ec5d9a1090446cc
Shourav Saha <shourav939@gmail.com>
2024-01-10 09:42:15 +0600

    just now i was learning how to update function work in query builder

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 
 class DemoController extends Controller
 {
-    //
+    //----------get data in data table ----------
     // public function DemoAction(){
     //     $advancedJoin = DB::table('product_carts')
     //     ->join('products', function(JoinClause $join){
@@ -19,7 +19,7 @@ class DemoController extends Controller
     //     return $advancedJoin;
     // }
 
-    // using union function //
+    // ----------using union function---------- //
     // public function unionFunction(){
     //     $query1 = DB::table('product_carts')->where('size', '=', 'x');
     //     $query2 = DB::table('product_carts')->where('product_id', '>', '4');
@@ -33,30 +33,30 @@ class DemoController extends Controller
         // where clause
         // ->where('color', 'like', '%gr%')
 
-        // where not clause
+        // ----------where not clause----------
         // ->whereNot('size', '<', 30)
 
-        // where between
+        // ----------where between----------
         // ->whereBetween('product_id', [1, 20])
 
-        // orderby function
+        // ----------orderby function----------
         // ->orderBy('color', 'asc')
 
-        // random order
+        // ----------random order----------
         // ->inRandomOrder()->first();
 
-        //skip and take value
+        //----------skip and take value----------
         // ->skip(1)
         // ->take(2)
         // ->skip(1)
         // ->take(1)
 
-        // group by and having function
+        // ----------group by and having function----------
         // ->groupBy('color')
         // ->having('product_id', '=', 3)
         // ->get();
 
-        // insert function
+        // ----------insert function----------
         // ->insert([
         //     'brandName' => 'demo brand',
         //     'brandImg' => 'demo image'
@@ -64,11 +64,20 @@ class DemoController extends Controller
         // return true;
     // }
 
-    // This is insert function
-    public function insert(Request $request){
+    // ----------This is insert function---------- //
+    // public function insert(Request $request){
+    //     DB::table('brands')
+    //     ->insert($request->input());
+    //     return true;
+    // }
+
+    // ----------this is update function----------
+    public function update(request $request){
         DB::table('brands')
-        ->insert($request->input());
+        ->where('id', '=', $request->id)
+        ->update($request->input());
         return true;
     }
+
 
 }
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::post("/insert", [DemoController::class, 'insert']);
+Route::post("/update/{id}", [DemoController::class, 'update']);
+
